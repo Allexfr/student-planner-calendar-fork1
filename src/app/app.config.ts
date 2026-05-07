9ebed30895648383169fdaae403bb0fe17914136
Nathan Bowman <gr8n8bowman1@gmail.com>
2026-05-06 22:53:43 -0500

    Installed Firebase and added Fire base config info

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,25 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// imported Firebase info above and angular router setup
+
+//below is the firebase project connection info
+const firebaseConfig = {
+  apiKey: "AIzaSyCqAyI-Kc1wVs0oh7GSBvApvQyyN9SJlms",
+  authDomain: "student-planner-calendar.firebaseapp.com",
+  projectId: "student-planner-calendar",
+  storageBucket: "student-planner-calendar.firebasestorage.app",
+  messagingSenderId: "257082706753",
+  appId: "1:257082706753:web:f6f6c912d8d7b349522fc0"
+};
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+  provideBrowserGlobalErrorListeners(),
+
+  provideRouter(routes),
+]
 };
