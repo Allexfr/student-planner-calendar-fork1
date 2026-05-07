bdcb4275e99d07b66f059101eb923179ac69d543
Nathan Bowman <gr8n8bowman1@gmail.com>
2026-05-06 22:26:02 -0500

    Added the basic landing pages and the routing for them

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { Calendar } from './pages/calendar/calendar';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  }, //Main landing page path
+
+  {
+    path: 'login',
+    component: Login
+  }, //Login page pathing
+
+  {
+    path: 'register',
+    component: Register
+  }, //User register page pathing
+
+  {
+    path: 'calendar',
+    component: Calendar
+  }, //Main calendar app page routing
+
+  {
+    path: '**',
+    redirectTo: ''
+  } //redirects errors back to home page unsure if keep or change
+];
